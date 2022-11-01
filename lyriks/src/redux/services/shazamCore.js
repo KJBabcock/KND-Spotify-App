178d06c0742998122ccb673dd900f1d493f9cee2
SquatchTang <kyjaba@gmail.com>
2022-10-31 20:12:43 -0500

    App started

---
+++ lyriks/src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'ce94283596msh972563346948845p1d94cajsn5b94022b5ee2',
+      'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    export const shazamCoreApi = createApi({
+        reducerPath: "shazamCoreApi",
+    });
